c342ec90e30610679eacfc1318502c2b2269dc8f
Sumrit <sumritgrover1@gmail.com>
2020-09-25 18:41:33 +0530

    Radio buttons added

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,36 +1,51 @@
 import React, { useState } from 'react';
-import {Text, View,StyleSheet, TouchableOpacity} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import RadioForm from 'react-native-simple-radio-button';
 
 
-export default SearchScreen =(props)=>{
-    const [search,setSearch]=useState();
-    const [details,setDetails]=useState();
-    return(
+export default SearchScreen = (props) => {
+    const [search, setSearch] = useState();
+    const [details, setDetails] = useState();
+
+    var srchtype = [
+        { id: "Aadhaar Name", label: "Aadhaar Name" },
+        { id: "DL Number", label: "DL Number" }
+    ]
+
+    return (
         <View style={styles.view}>
-            <View style={{marginTop:200,margin:20,backgroundColor:"#ffffff",borderRadius:20}}>
-                <TextInput
-                placeholder='Search By'
-                style={styles.textinput1}
-                onChangeText={(res)=>{
-                    setSearch(res)
-                }}
+            <View style={{ marginTop: 200, margin: 20, backgroundColor: "#ffffff", borderRadius: 20 }}>
+                <Text style={styles.textinput1}>
+                    Search By
+                </Text>
+                <RadioForm
+                    style={{ marginLeft: 10, marginBottom: -10 }}
+                    radio_props={srchtype}
+                    selectedButtonColor={"coral"}
+                    buttonColor={"#777777"}
+                    labelColor={"#777777"}
+                    selectedLabelColor={"#777777"}
+                    buttonSize={20}
+                    onPress={(val) => {
+                        setSearch(val)
+                    }}
                 />
                 <TextInput
-                placeholder='Enter details of selected category'
-                style={styles.textinput2}
-                onChangeText={(res)=>{
-                    setDetails(res)
-                }}
+                    placeholder='Enter details of selected category'
+                    style={styles.textinput2}
+                    onChangeText={(res) => {
+                        setDetails(res)
+                    }}
                 />
             </View>
             <TouchableOpacity
-            style={styles.button}
-            onPress={()=>{
-                props.navigation.navigate("displayScreen")
-            }}
+                style={styles.button}
+                onPress={() => {
+                    props.navigation.navigate("displayScreen")
+                }}
             >
-                <Text style={{fontSize:30,textAlign:"center",color:'#f1f0f2'}}>
+                <Text style={{ fontSize: 30, textAlign: "center", color: '#f1f0f2' }}>
                     Search
                 </Text>
             </TouchableOpacity>
@@ -38,27 +53,28 @@ export default SearchScreen =(props)=>{
     )
 }
 
-const styles=StyleSheet.create({
-    textinput1:{
-        marginTop:30,
-        borderBottomWidth:1,
-        margin:10,
-        borderColor:'lightgrey'
+const styles = StyleSheet.create({
+    textinput1: {
+        marginTop: 30,
+        // borderBottomWidth: 1,
+        margin: 10,
+        borderColor: 'lightgrey',
+        color:'grey'
     },
-    view:{
-        backgroundColor:'#f1f0f2'
+    view: {
+        backgroundColor: '#f1f0f2'
     },
-    textinput2:{
-        marginBottom:30,
-        borderBottomWidth:1,
-        margin:10,
-        borderColor:'lightgrey'
+    textinput2: {
+        marginBottom: 30,
+        borderBottomWidth: 1,
+        margin: 10,
+        borderColor: 'lightgrey'
     },
-    button:{
-        backgroundColor:'coral',
-        marginLeft:100,
-        marginRight:100,
-        borderRadius:10,
-        marginTop:30
+    button: {
+        backgroundColor: 'coral',
+        marginLeft: 100,
+        marginRight: 100,
+        borderRadius: 10,
+        marginTop: 30
     }
 })
